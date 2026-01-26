4650e275214a0e89a6381fc3590b23413528f3d1
EDIMO-tech <mr.edimo2004@gmail.com>
2026-01-26 21:39:12 -0100

    Update connection to use PostgreSQL instead of MySQL

--- db.php
+++ db.php
@@ -1,12 +1,18 @@
 <?php
-$host = "localhost";
-$user = "root";   // ton utilisateur MySQL
-$pass = "";       // ton mot de passe MySQL
-$db   = "mabase";
+// Infos de connexion Render PostgreSQL
+$host = "dpg-d5rpjiv5r7bs739ciaig-a.oregon-postgres.render.com";       // ex: dpg-xxxx.oregon-postgres.render.com
+$port = "5432";                  // port par défaut PostgreSQL
+$dbname = "mabase_u0z7";              // nom de ta base
+$user = "mabase";                // ton utilisateur
+$pass = "s55zgv2gSSBzAiZ0KPb7CQkv2bBo0063"; // ton mot de passe
 
-$conn = new mysqli($host, $user, $pass, $db);
+// Connexion PostgreSQL
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
 
-if ($conn->connect_error) {
-    die("Erreur de connexion : " . $conn->connect_error);
-}
+if (!$conn) {
+    die("Erreur de connexion à la base PostgreSQL.");
+} 
+// Sinon connexion OK
 ?>
+
+
