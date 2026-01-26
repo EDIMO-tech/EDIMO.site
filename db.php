0c1a9c408df9791a78af53cbcbf14ce76f3411f4
EDIMO-tech <mr.edimo2004@gmail.com>
2026-01-26 21:49:25 -0100

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,18 @@
 <?php
-// Infos de connexion Render PostgreSQL
-$host = "dpg-d5rpjiv5r7bs739ciaig-a.oregon-postgres.render.com";       // ex: dpg-xxxx.oregon-postgres.render.com
-$port = "5432";                  // port par défaut PostgreSQL
-$dbname = "mabase_u0z7";              // nom de ta base
-$user = "mabase";                // ton utilisateur
-$pass = "s55zgv2gSSBzAiZ0KPb7CQkv2bBo0063"; // ton mot de passe
+$host = "dpg-d5rpjiv5r7bs739ciaig-a.oregon-postgres.render.com";
+$port = "5432";
+$dbname = "mabase";
+$user = "mabase";
+$pass = "TonMotDePasseRender";
 
-// Connexion PostgreSQL
-$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
-
-if (!$conn) {
-    die("Erreur de connexion à la base PostgreSQL.");
-} 
-// Sinon connexion OK
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Connexion OK
+} catch (PDOException $e) {
+    die("Erreur de connexion PostgreSQL : " . $e->getMessage());
+}
 ?>
+
 
 
